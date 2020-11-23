ae44aec17132e2ec6f5b32cd74d6a3b16683f456
aiya000 <aiya000.develop@gmail.com>
2020-11-23 16:26:11 +0900

    Make scripts basis

---
+++ Assets/galaxy-sixth-sensey/Flya/Scripts/Flyer.cs
@@ -0,0 +1,88 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
+using VRC.Udon;
+
+public class Flyer : UdonSharpBehaviour {
+    private readonly int WAITING_COUNT = 50;
+
+    /// <summary>
+    /// The owner of Flyer by pickupping.
+    ///
+    /// Being null means this object is not picked up now.
+    /// </summary>
+    private VRCPlayerApi owner;
+
+    /// <summary>
+    /// A limitation for EmulateFlying() executions.
+    /// </summary>
+    private int countToSkip = 0;
+
+    private Vector3 latestOwnerPos = Vector3.zero;
+
+    /// <summary>
+    /// Initailizes this instance to fly.
+    /// </summary>
+    public override void OnPickup() {
+        this.owner = Networking.LocalPlayer;
+        this.countToSkip = WAITING_COUNT;
+        this.latestOwnerPos = this.transform.position;
+    }
+
+    /// <summary>
+    /// Initializes this instance to stop.
+    /// </summary>
+    public override void OnDrop() {
+        this.owner = null;
+        this.countToSkip = WAITING_COUNT;
+        this.latestOwnerPos = Vector3.zero;
+    }
+
+    // public override void OnPickupUseDown()
+    // {
+    // }
+
+    public void FixedUpdate() {
+        if (this.owner == null) {
+            return;
+        }
+
+        this.EmulateFlying();
+    }
+
+    /// <summary>
+    /// Emulates flying sky.
+    ///
+    /// Requirement:
+    /// - this.owner != null
+    /// </summary>
+    private void EmulateFlying() {
+        if (this.owner.IsPlayerGrounded()) {
+            return;
+        }
+
+        if (!this.IsReadyToFixY()) {
+            this.owner.SetVelocity(this.latestOwnerPos);  // Keep the position
+            this.countToSkip--;
+            return;
+        }
+
+        // var nextY = Mathf.Lerp(_fallStartedY, transform.position.y, 6 * Time.fixedDeltaTime);
+        var nextPosition = new Vector3(0, this.latestOwnerPos.y - 0.1f, 0);
+        this.owner.SetVelocity(nextPosition);
+
+        // Wait to fly again.
+        this.latestOwnerPos = this.transform.position;
+        this.countToSkip = WAITING_COUNT;
+    }
+
+    private bool IsReadyToFixY() {
+        if (this.countToSkip < 0) {
+            Debug.Log($"Flyer: IsReadyToFixY(): Something wrong. this.countToSkip is: {this.countToSkip}");
+            return false;
+        }
+
+        return (this.countToSkip != 0);
+    }
+}
---
+++ Assets/galaxy-sixth-sensey/Flya/Scripts/RespawingItems.cs
@@ -0,0 +1,35 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RespawingItems : UdonSharpBehaviour {
+    [SerializeField]
+    private Transform[] items;
+
+    private Vector3[] initialItemsPositions;
+
+    public void Start() {
+        if (this.items == null) {
+            Debug.Log("RespawingItems: Start(): this.items is not set.");
+            return;
+        }
+
+        this.initialItemsPositions = new Vector3[this.items.Length];
+
+        for (var i = 0; i < this.items.Length; i++) {
+            this.initialItemsPositions[i] = this.items[i].position;
+        }
+    }
+
+    public override void Interact() {
+        if (this.items == null) {
+            Debug.Log("RespawingItems: Interact(): this.items is not set.");
+            return;
+        }
+
+        for (var i = 0; i < this.items.Length; i++) {
+            this.items[i].position = this.initialItemsPositions[i];
+        }
+    }
+}
